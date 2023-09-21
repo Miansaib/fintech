3d601fa1fa1cea869d5eef1907323bc485e4a5f9
xerodays <sayedidrees47@gmail.com>
2023-09-21 22:13:08 +0500

    assets folder set

--- lib/Screens/Auth/LoginScreen.dart
+++ lib/Screens/Auth/LoginScreen.dart
@@ -6,62 +6,63 @@ import 'package:flutter/material.dart';
 import '../../Constants/ContainerShadow.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
+
 class _LoginScreenState extends State<LoginScreen> {
-  final formKey= GlobalKey<FormState>();
-  TextEditingController emailController=TextEditingController();
-  TextEditingController passwordController=TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         height: size.height,
         width: size.width,
         decoration: BoxDecoration(
-          
-          gradient:  splashScreenGradient,
+          gradient: splashScreenGradient,
         ),
         child: Center(
           child: Container(
-            padding: EdgeInsets.only(left: 80,right: 80),
-            height: size.height*0.7,
-            width: size.width*0.28,
+            padding: EdgeInsets.only(left: 80, right: 80),
+            height: size.height * 0.7,
+            width: size.width * 0.28,
             decoration: BoxDecoration(
-              border: Border.all(
-            width: 1,
-            color: Colors.black
-          ),
-          boxShadow: [ContainerShadow().containerShadow],
-              color: Color(0xffEDFBF2),
-              borderRadius: BorderRadius.circular(61)
-            ),
+                border: Border.all(width: 1, color: Colors.black),
+                boxShadow: [ContainerShadow().containerShadow],
+                color: Color(0xffEDFBF2),
+                borderRadius: BorderRadius.circular(61)),
             child: Form(
               key: formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // Image(
-                  //   height: 450,
-                  //   width: 450,
-                  //   image: AssetImage('Assets/PNG/as.png')),
-                  CustomTextFormField(controller: emailController, hint: 'Email', label: 'Email'),
-                  CustomTextFormField(controller: passwordController, hint: 'Password', label: 'Password'),
-                  Row(mainAxisAlignment: MainAxisAlignment.end,children: [
-                    ForgotPasswordText(),
-                  ],)
+                  Image(width: 100, image: AssetImage('Assets/PNG/as.png')),
+                  CustomTextFormField(
+                      controller: emailController,
+                      hint: 'Email',
+                      label: 'Email'),
+                  CustomTextFormField(
+                      controller: passwordController,
+                      hint: 'Password',
+                      label: 'Password'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ForgotPasswordText(),
+                    ],
+                  )
                 ],
               ),
             ),
           ),
         ),
       ),
-      
     );
   }
 }
